345241098a9213567a2e321465a73346c7a20d6a
Ph-uad <temidayo26j@gmail.com>
2022-10-25 12:13:04 +0100

    revamping

--- src/components/apartment/Apartment.js
+++ src/components/apartment/Apartment.js
@@ -1,13 +1,47 @@
-import React from 'react';
-import ApartmentCard from './ApartmentCards';
+import Module from './Module';
+import Classes from './apartment.module.css';
+import Family from '../../Assets/img/plan__family.jpg'
+import Office from '../../Assets/img/plan__office.jpg'
+import Vacation from '../../Assets/img/plan__vacation.jpg'
+import { useState } from 'react';
 
-const Apartment = () => {
+const Plan = () => {
+
+        const [initialSlide, setCurrentSlide] = useState(0);
+
+        const CardData= [
+            {img : Family, title : "Your Plan To Build A home"},
+            {img : Office, title : "Your Plan To Build A work space"},
+            {img : Vacation, title : "Your Plan To Build A Vacation"},
+        ]
+
+        const previousSlide = () =>{
+            let isFirstSlide =  initialSlide === 0;
+            let newSlide = isFirstSlide ? CardData.length - 1 : initialSlide - 1
+            setCurrentSlide(newSlide);
+        }
+        const nextSlide = () =>{
+            let isFirstSlide =  initialSlide === CardData.length - 1;
+            let newSlide = isFirstSlide ? 0 : initialSlide + 1
+            setCurrentSlide(newSlide);
+        }
+
     return (
-        <section>
+        <section className={Classes.container}>
+
             <h4 className="heading-quadruple">best deals</h4>
             <h2 className=" heading-secondary bolder">Your Plan </h2>
-            <ApartmentCard/>         
+
+            <div className={Classes.card}>
+                <Module data={CardData[initialSlide]}/>
+            </div>
+
+            <div className='flex'>
+                <button className={`${Classes['navigation__elipse--left']} ${Classes.btn}`} onClick={previousSlide}>L</button>
+                <button className={`${Classes['navigation__elipse--right']} ${Classes.btn}`} onClick={nextSlide} >R</button>
+            </div>
+
         </section>
     )
 }
-export default Apartment;
+export default Plan;
---
+++ src/components/apartment/Module.js
@@ -0,0 +1,24 @@
+import Classes from './apartment.module.css';
+
+
+const Module = ({data}) => {
+    console.log(data)
+    return (
+        // <div className={`  ${Classes["container"]}`}>
+        <div className={`mod flex ${Classes.module}`}>
+            <figure className={Classes.figure}>
+                <img src={data.img} className={Classes['image']} alt="house"></img>
+            </figure>
+            <div className={Classes['details']}>
+                <h2 className='heading--secondary bolder'>{data.title}</h2>
+                <p className='paragraph'>
+                    Maintainable and sustainable properties that are
+                    structured to host a family still cozy. In nice neighbourhood
+                    strategically placed within range of utility stores, schools,
+                    leisure parks and some other fun things the city has in store.
+                </p>
+            </div>
+        </div>
+    )
+}
+export default Module;
