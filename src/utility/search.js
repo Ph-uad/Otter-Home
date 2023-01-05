fb06fc9fb4aca0acec206a745bb0aa4862228cf7
Ph-uad <temidayo26j@gmail.com>
2023-01-05 20:52:10 +0100

    search

--- src/utility/search.js
+++ src/utility/search.js
@@ -9,7 +9,6 @@ const Search = ({list, searchResult}) =>{
         const resultArray = list.filter(item => item.location.toLowerCase().includes(e.target.value) || item.description.toLowerCase().includes(e.target.value));
         searchResult(resultArray)
     }
-
     return(
         <form className="search" onSubmit={submitHandler}>
                <input spellCheck="false" className="search__input width--mid" type="text" placeholder="Search properties by locaton..." onChange={onChangeHandler} />
