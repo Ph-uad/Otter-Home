6d9286beaddbdd7f7c6e82008a5bc4ff8232fe7a
Ph-uad <temidayo26j@gmail.com>
2023-01-24 04:20:54 +0100

    icons

--- src/components/layout/navigation/Nav.js
+++ src/components/layout/navigation/Nav.js
@@ -12,13 +12,13 @@ function Nav() {
             </span>
             <ul className={ `${Classes['list']} flex` }>
                 <li className={ `flex ${Classes['list-link']}` }>
-                    <NavLink className={ ({ isActive }) => (isActive ? 'current flex' : 'flex') } to='/'><SVG urlId="#icon-building"/> <span>Home </span> </NavLink>
+                    <NavLink className={ ({ isActive }) => (isActive ? 'current flex' : 'flex') } to='/'><SVG urlId="#icon-home"/> <span>Home </span> </NavLink>
                 </li>
                 <li className={ `flex ${Classes['list-link']}` }>
                     <NavLink className={ ({ isActive }) => (isActive ? 'current flex' : 'flex') } to='/listing'><SVG urlId="#icon-building"/> <span>Listing </span></NavLink>
                 </li>
                 <li className={ `flex ${Classes['list-link']}` }>
-                    <NavLink className={ ({ isActive }) => (isActive ? 'current flex' : 'flex') } to='/about'><SVG urlId="#icon-building"/><span> About us</span></NavLink>
+                    <NavLink className={ ({ isActive }) => (isActive ? 'current flex' : 'flex') } to='/about'><SVG urlId="#icon-info"/><span> About us</span></NavLink>
                 </li>
             </ul>
 
