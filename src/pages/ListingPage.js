af91752682c3347c03bc7a8eac95b029666334d4
Ph-uad <temidayo26j@gmail.com>
2023-02-08 04:39:52 +0100

    styling

--- src/pages/ListingPage.js
+++ src/pages/ListingPage.js
@@ -3,9 +3,7 @@ import Listing from "../components/layout/listing/Listing";
 const ListingPage = () => {
   return (
       <section className="listing section">
-        <div className="width--mid">
           <Listing />
-        </div>
       </section>
   )
 }
