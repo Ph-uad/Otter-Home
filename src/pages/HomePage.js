85d882596aa58c310905055bf9399e59213f5e07
Ph-uad <temidayo26j@gmail.com>
2023-02-11 23:46:02 +0100

    homePage revamp

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,16 +1,8 @@
 import Header from "../components/layout/header/Header";
-// import Attributes from "../components/layout/poster/Attribute1";
-// import Caption from "../components/layout/poster/Caption";
-// import Testimonial from "../components/layout/poster/Testimonial";
 
 function HomePage() {
     return (
-        <>
             <Header />
-            {/* <Attributes/>
-            <Caption/>   
-            <Testimonial/>          */}
-        </>
     );
 }
 
