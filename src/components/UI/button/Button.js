0ca1a8e7b66ab5e9f1f279486463d1c2f5e55ee8
Ph-uad <temidayo26j@gmail.com>
2023-02-13 01:25:56 +0100

    responsive fix

--- src/components/UI/button/Button.js
+++ src/components/UI/button/Button.js
@@ -4,7 +4,7 @@ import classes from './button.module.css'
 export function Button(props){
     const {href, type, label, } = props
     if (href){
-     return <Link to={href} className={`${classes.button} ${classes["button-primary"]} link`} role="anchor link">{label}</Link>
+     return <Link to={href} className={`button ${classes["button-primary"]} link`} role="anchor link">{label}</Link>
     }
     return <button aria-label={label} className={`button ${classes["button-primary"]}`} type={type ? type : "submit"}>{props.children}</button>
 }
