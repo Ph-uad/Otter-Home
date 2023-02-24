ad3d1fa434250e55f668d680bc6a47a1c7077b31
Ph-uad <temidayo26j@gmail.com>
2023-02-24 04:51:08 +0100

    fixing button style and grid style

--- src/components/UI/button/Button.js
+++ src/components/UI/button/Button.js
@@ -4,7 +4,7 @@ import classes from './button.module.css'
 export function Button(props){
     const {href, type, label, } = props
     if (href){
-     return <Link to={href} className={` ${classes["button-primary"]} button link`} role="anchor link">{label}</Link>
+     return <Link to={href} className={` ${classes["button-primary"]} ${classes.button} link`} role="anchor link">{label}</Link>
     }
-    return <button aria-label={label} className={`button ${classes["button-primary"]}`} type={type ? type : "submit"}>{props.children}</button>
+    return <button aria-label={label} className={`${classes.button} ${classes["button-primary"]}`} type={type ? type : "submit"}>{props.children}</button>
 }
